c2120dd22dc5e8a1d8ad8333c46470e9d4114097
oriolgut <oriol.gut@noser.com>
2017-11-03 11:29:07 +0100

    added menu controller

---
+++ TBZ_M151_RestaurantPlanner/RestaurantPlanner/Controllers/MenusController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RestaurantPlanner.DAL;
+using RestaurantPlanner.Models;
+
+namespace RestaurantPlanner.Controllers
+{
+    public class MenusController : Controller
+    {
+        private readonly RestaurantPlannerContext _db = new RestaurantPlannerContext();
+
+        // GET: Menus
+        public ActionResult Index()
+        {
+            return View(_db.Menus.ToList());
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

cf5544865f1071042d019d89ea8cfa5a27026005
oriolgut <oriol.gut@noser.com>
2017-11-16 18:59:46 +0100

    added dto for gericht controller

---
+++ TBZ_M151_RestaurantPlanner/RestaurantPlanner/DataTransferObjects/GerichtDataTransferObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantPlanner.DataTransferObjects
+{
+    public class GerichtDataTransferObject
+    {
+        public int GerichtId { get; set; }
+
+        [Required]
+        public string GerichtName { get; set; }
+
+        [Required, Range(minimum: 0.1D, maximum: double.MaxValue)]
+        public double GerichtPreis { get; set; }
+
+        [Required]
+        public string MenuZugehoerigkeit { get; set; }
+
+    }
+}
